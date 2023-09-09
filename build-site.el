fc063246366bbb73bc38015025e95a0f558797a4
GerMP21 <germanmarpar@gmail.com>
2023-09-09 02:07:53 -0300

    Got the basic system working

--- build-site.el
+++ build-site.el
@@ -1,6 +1,12 @@
 ;; Load the publishing system
 (require 'ox-publish)
 
+;; Customize the HTML output
+(setq org-html-validation-link nil            ;; Don't show validation link
+      org-html-head-include-scripts nil       ;; Use our own scripts
+      org-html-head-include-default-style nil ;; Use our own styles
+      org-html-head "<link rel=\'stylesheet\' href=\'./styles/simple.css\' />")
+
 ;; Define the publishing project
 (setq org-publish-project-alist
       (list
@@ -8,6 +14,11 @@
              :recursive t
              :base-directory "./content"
              :publishing-directory "./public"
+             :with-author t             ;; Don't include author name
+             :with-creator t            ;; Include Emacs and Org versions in footer
+             :with-toc nil              ;; Include a table of contents
+             :section-numbers nil       ;; Don't include section numbers
+             :time-stamp-file nil       ;; Don't include time stamp in file
              :publishing-function 'org-html-publish-to-html)))
 
 ;; Generate the site output
